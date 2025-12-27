c627a9e7f5a801206e663a92106c5c8fcc570215
N2theFray <blacksteven1@gmail.com>
2025-12-27 15:06:17 -0500

    chore: download all intro bites

---
+++ Intro/f-strings-and-a-simple-ifelse/driving.py
@@ -0,0 +1,11 @@
+MIN_DRIVING_AGE = 18
+
+
+def allowed_driving(name, age):
+    """Print '{name} is allowed to drive' or '{name} is not allowed to drive'
+       checking the passed in age against the MIN_DRIVING_AGE constant"""
+
+
+if __name__ == '__main__':
+    allowed_driving('John', 17)
+    allowed_driving('Sara', 22)
---
+++ Intro/type-conversion-and-exception-handling/division.py
@@ -0,0 +1,9 @@
+def divide_numbers(numerator, denominator):
+    """
+    For this exercise you can assume numerator and denominator are of type int/str/float.
+    Try to convert numerator and denominator to int types, if that raises a
+    ValueError reraise it with the message "Can only divide two numbers".
+    Following do the division and return the result.
+    However if denominator is 0 catch the corresponding Python exception that
+    is thrown and return 0.
+    """
